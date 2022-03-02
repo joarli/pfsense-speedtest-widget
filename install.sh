ee5490a01acb58be6d6d8819af99c16c3bfb8ae3
Joarli Leandro <joarli.leandro@gmail.com>
2022-03-02 14:30:17 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,3 +14,8 @@ if [ "$(cat /etc/version | cut -c 1-3)" == "21." ]; then #pfSense PLUS Edition
         pkg install -y py38-speedtest-cli-2.1.3
         fetch -q -o /usr/local/www/widgets/widgets/speedtest.widget.php https://raw.githubusercontent.com/joarli/pfsense-speedtest-widget/master/speedtest.widget.php
 fi
+
+if [ "$(cat /etc/version | cut -c 1-3)" == "2.6" ]; then #New Edition
+        pkg install -y py38-speedtest-cli-2.1.3
+        fetch -q -o /usr/local/www/widgets/widgets/speedtest.widget.php https://raw.githubusercontent.com/joarli/pfsense-speedtest-widget/master/speedtest.widget.php
+fi
