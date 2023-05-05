38a7e6f7b72f62d3749af10bde5ace2225eca979
Joarli Leandro <joarli.leandro@gmail.com>
2023-05-05 16:40:47 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ if [ "$(cat /etc/version | cut -c 1-3)" == "2.5" ]; then
 fi
 #VERSAO COMMUNITY ATUAL
 if [ "$(cat /etc/version | cut -c 1-3)" == "2.6" ]; then #New Edition
-        pkg install -y py39-speedtest-cli-2.1.3
+        pkg install -y py38-speedtest-cli-2.1.3
         fetch -q -o /usr/local/www/widgets/widgets/speedtest.widget.php https://raw.githubusercontent.com/joarli/pfsense-speedtest-widget/master/speedtest.widget.php
 fi
 #VERSAO PF+
