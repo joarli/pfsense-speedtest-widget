423275a6f4c6a298c864649f5939fbae120621ea
Joarli Leandro <joarli.leandro@gmail.com>
2025-08-19 08:28:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,8 +14,13 @@ if [ "$(cat /etc/version | cut -c 1-3)" == "2.6" ]; then
         pkg install -y py38-speedtest-cli-2.1.3
         fetch -q -o /usr/local/www/widgets/widgets/speedtest.widget.php https://raw.githubusercontent.com/joarli/pfsense-speedtest-widget/master/speedtest.widget.php
 fi
-#VERSAO COMMUNITY ATUAL NOVO SPEEDTEST-CLI
 if [ "$(cat /etc/version | cut -c 1-3)" == "2.7" ]; then #New Edition
+        echo "instalando versao speedtest"
+        pkg install -y py311-speedtest-cli-2.1.3
+        fetch -q -o /usr/local/www/widgets/widgets/speedtest.widget.php https://raw.githubusercontent.com/joarli/pfsense-speedtest-widget/master/speedtest.widget.php
+fi
+#VERSAO COMMUNITY ATUAL NOVO SPEEDTEST-CLI
+if [ "$(cat /etc/version | cut -c 1-3)" == "2.8" ]; then #New Edition
         echo "instalando versao speedtest"
         pkg install -y py311-speedtest-cli-2.1.3
         fetch -q -o /usr/local/www/widgets/widgets/speedtest.widget.php https://raw.githubusercontent.com/joarli/pfsense-speedtest-widget/master/speedtest.widget.php
@@ -34,6 +39,11 @@ if [ "$(cat /etc/version | cut -c 1-3)" == "23." ]; then #pfSense PLUS Edition
         fetch -q -o /usr/local/www/widgets/widgets/speedtest.widget.php https://raw.githubusercontent.com/joarli/pfsense-speedtest-widget/master/speedtest.widget.php
 fi
 if [ "$(cat /etc/version | cut -c 1-3)" == "24." ]; then #pfSense PLUS Edition
+        pkg install -y py311-speedtest-cli-2.1.3
+        fetch -q -o /usr/local/www/widgets/widgets/speedtest.widget.php https://raw.githubusercontent.com/joarli/pfsense-speedtest-widget/master/speedtest.widget.php
+fi
+#VERSAO PF+ ATUAL
+if [ "$(cat /etc/version | cut -c 1-3)" == "25." ]; then #pfSense PLUS Edition
         pkg install -y py311-speedtest-cli-2.1.3
         fetch -q -o /usr/local/www/widgets/widgets/speedtest.widget.php https://raw.githubusercontent.com/joarli/pfsense-speedtest-widget/master/speedtest.widget.php
 fi
